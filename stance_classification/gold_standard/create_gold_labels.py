aba390c92f53849dc0d449ae1bf41abdcb55d3ca
ronpik <ronpi1310@gmail.com>
2020-01-11 19:39:41 +0200

    scrolling trials

--- stance_classification/gold_standard/create_gold_labels.py
+++ stance_classification/gold_standard/create_gold_labels.py
@@ -4,19 +4,15 @@ from typing import Iterable, Tuple, List, Dict
 
 import networkx as nx
 
-from stance_classification.community.community_detection import plot_partitions_score, spatial_bipartition, find_communities
-from stance_classification.community.community_utils import get_op_community
+
 from stance_classification.draw_utils import draw_tree
 from stance_classification.graph_utils import inter_communication_index, tree_to_graph
 from stance_classification.classifiers.greedy_stance_classifier import MSTStanceClassifier
-from stance_classification.classifiers.maxcut import max_cut, draw_maxcut
 from stance_classification.classifiers.maxcut_stance_classifier import MaxcutStanceClassifier
 from stance_classification.classifiers.random_stance_classifier import RandomStanceClassifier
-from stance_classification.user_interaction.user_interaction_parser import parse_users_interactions, UsersInteraction, \
-    cmv_interaction_weight
-from stance_classification.user_interaction.users_interaction_graph import build_users_interaction_graph, \
-    draw_user_interactions_graph, \
-    to_undirected_gaprh, get_core_interactions
+from stance_classification.user_interaction.user_interaction_parser import parse_users_interactions, UsersInteraction
+from stance_classification.user_interaction.users_interaction_graph import draw_user_interactions_graph, \
+     get_core_interactions
 from stance_classification.utils import iter_trees_from_jsonl, skip_elements
 
 DISAGREEMENT_TAGS = {"CBE", "CBD", "CBF", "OCQ", "DNO"}   #CBB - ad hominem
@@ -56,14 +52,14 @@ def show_labels(trees: Iterable[dict]):
     compare_classifiers = True
     compute_communities = False
 
-    num_skip = 4
+    num_skip = 0
     skip_elements(trees, num_skip)
     for i, tree in enumerate(trees, num_skip):
         op = tree["node"]["author"]
         if op == "[deleted]":
             continue
 
-        print(f"Tree: {i} ; OP: {op} ; Title: {tree['node']['extra_data']['title']}")
+        # print(f"Tree: {i} ; OP: {op} ; Title: {tree['node']['extra_data']['title']}")
         interactions = parse_users_interactions(tree, anonimous=anonimous)
         pairs_labels = extract_labeled_pairs(interactions)
         for user1, user2, all_pair_labels in pairs_labels:
@@ -117,6 +113,7 @@ def show_labels(trees: Iterable[dict]):
             draw_user_interactions_graph(undir_graph, op=op, use_weight=True, title=title)
 
         if compare_classifiers:
+
             rsc = RandomStanceClassifier()
             rsc.set_input(undir_graph)
             rsc.classify_stance(op, 1./3)
@@ -133,17 +130,20 @@ def show_labels(trees: Iterable[dict]):
             maxcut_clf.set_input(undir_graph)
             try:
                 maxcut_clf.classify_stance(op)
+                trees_index[len(trees_index)] = tree["node"]["id"]
+                print(f"{len(trees_index)},{tree['node']['id']}")
                 if show_graph:
                     maxcut_clf.draw()
-
 
 
             except OverflowError as e:
                 print(e)
 
 
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
+    trees_index = {}
     # labeled_trees_path = "/home/ron/data/bgu/labeled/labeled_trees.jsonl"
     labeled_trees_path = "/home/ron/data/bgu/labeled/61019_notcut_trees.txt"
     trees = iter_trees_from_jsonl(labeled_trees_path)
